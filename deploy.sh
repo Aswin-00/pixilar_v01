4967c2e0adf428aeda1efe8bb6d9634a828654ae
Aswin-00 <aswin.23pmc114@mariancollege.org>
2024-08-05 00:37:27 +0530

    madechagedeployscript

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ echo "deleting old app"
 sudo rm -rf /var/www/
 
 echo "creating app folder"
-sudo mkdir -p /var/www/langchain-app 
+sudo mkdir -p /var/www/pixilar
 
 echo "moving files to app folder"
-sudo mv  * /var/www/langchain-app
+sudo mv  * /var/www/pixilar
 
 # Navigate to the app directory
-cd /var/www/langchain-app/
+cd /var/www/pixilar/
 sudo mv env .env
 
 sudo apt-get update

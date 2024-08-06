e6f085a6a0055fab4df3d8f1a04143156c7a4eaf
Aswin-00 <aswin.23pmc114@mariancollege.org>
2024-08-06 16:22:55 +0530

    modified_gunicorn_service

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,10 @@
 sudo apt-get update
 # sudo apt-get -y install nginx python3.11 python3.11-venv  python3.11-full
 
-sudo apt-get -y install  python3.11 python3.11-venv python3-pip nginx
+sudo apt-get install  python3.11 
+sudo apt-get install python3.11-venv 
+sudo apt-get install python3-pip 
+sudo apt-get install  nginx
 
 python3.11  -m venv box_venv
 

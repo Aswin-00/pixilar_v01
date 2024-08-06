01da6131ab20d69a42859a4124570b78e1fbdd52
Aswin-00 <aswin.23pmc114@mariancollege.org>
2024-08-06 13:37:38 +0530

    changes_to_gunicorn_to_service

--- deploy.sh
+++ deploy.sh
@@ -7,27 +7,27 @@
 # # Move the 'env' directory to '.env' if needed (rename for convention purposes).
 # # Uncomment if you need to rename an existing virtual environment directory.
 
-# #sudo systemctl stop gunicorn.service
+#sudo systemctl stop gunicorn.service
 
 # sudo mv env .env
 # # Update package lists for upgrades and new package installations
-# sudo apt-get update
-# sudo apt-get -y install python3.11 python3.11-venv  python3.11-full
+sudo apt-get update
+sudo apt-get -y install python3.11 python3.11-venv  python3.11-full
 
-# python3.11  -m venv box_venv
+python3.11  -m venv box_venv
 
 
 # # Activate the virtual environment
-# source box_venv/bin/activate
+source box_venv/bin/activate
 
 # # Install all required packages listed in 'requirements.txt'
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
 # # Install Gunicorn, the WSGI server, if it's not listed in requirements.txt
 
 # # Make database migrations (prepares migration files)
 
-# python3.11 manage.py add_product
+python3.11 manage.py add_product
 
 sudo systemctl daemon-reload
 
